aa8e8f75dc0466e328ab0d24c2a60be4362c093b
PCdevv <putericitradev@gmail.com>
2023-07-31 23:03:03 +0700

    add HelloWorld & Biodata

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class HelloWorld
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine('p');
+        Console.WriteLine("Hello World");
+        Console.Write("Nama :");
+        Console.Write("Puteri Citra Dewi");
+        Console.WriteLine("===================");
+        Console.WriteLine("HIHEHIHA");
+    }
+}
---
+++ biodata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ProgramInput
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("=== PROGRAM BIODATA ===");
+        Console.Write("Tuliskan nama kamu : ");
+        string nama = Console.ReadLine();
+        Console.Write("Alamat : ");
+        string alamat = Console.ReadLine();
+        Console.Write("Jurusan : ");
+        string jurusan = Console.ReadLine();
+        Console.Write("Cita-cita saya : ");
+        string cita = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Biodata Saya");
+        Console.WriteLine("Nama saya        : " + nama);
+        Console.WriteLine("Alamatnya di     : " + alamat);
+        Console.WriteLine("Jurusanku adalah : " + jurusan);
+        Console.WriteLine("Cita-cita saya   : " + cita);
+    }
+}
