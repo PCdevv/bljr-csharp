49227c56bc2884cea69e49a2e0504726103a2feb
PCdevv <putericitradev@gmail.com>
2023-08-01 07:58:25 +0700

    add luas lingkaran

---
+++ LuasLingkaran/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class LuasLingkaran
+{
+    public static void Main(string[] args)
+    {
+        const float Phi = 3.14f;
+
+        Console.WriteLine("===Program Luas Lingkaran===");
+        Console.Write("Input jari-jari :");
+        int r = int.Parse(Console.ReadLine());
+
+        var luas = Phi * r * r;
+
+        Console.WriteLine($"Luas Lingkaran : {luas}");
+    }
+}
